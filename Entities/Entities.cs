187dc78f831358d29a5eb62d6bcf64ef9b802d6d
Othman Al-Moussawel <57558743+osman-developer@users.noreply.github.com>
2021-02-10 08:49:00 +0200

    Add files via upload

---
+++ Entities/Entities.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Entities
+    {
+        public class Params_Delete_Customer
+        {
+            public int CUSTOMER_ID { get; set; }
+        }
+
+        public class Params_Delete_Booking
+        {
+            public int BOOKING_ID { get; set; }
+        }
+
+        public class Params_Delete_Court
+        {
+            public int COURT_ID { get; set; }
+        }
+        public class Currency
+        {
+            public int CURRENCY_ID { get; set; }
+            public string NAME { get; set; }
+            public string SYMBOL { get; set; }
+        }
+        public class Booking
+        {
+            public int BOOKING_ID { get; set; }
+            public int CUSTOMER_ID { get; set; }
+            public int COURT_ID { get; set; }
+            public DateTime DATE { get; set; }
+            public int HOUR { get; set; }
+            public int DURATION { get; set; }
+            public string DESCRIPTION { get; set; }
+            public Boolean IS_DONE { get; set; }
+            public Boolean IS_PAID { get; set; }
+            public decimal AMOUNT { get; set; }
+            public int CURRENCY_ID { get; set; }
+
+        }
+        public class Customer
+        {
+            public int CUSTOMER_ID { get; set; }
+            public string FIRST_NAME { get; set; }
+
+            public string LAST_NAME { get; set; }
+            public string MOBILE { get; set; }
+        }
+        public class Court
+        {
+            public int COURT_ID { get; set; }
+            public string NAME { get; set; }
+
+            public string ADDRESS { get; set; }
+        }
+    }
+}
